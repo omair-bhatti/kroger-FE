640b8a96fdb7336380ee72a7af354b1bc58022e4
omair-chopdawg <omair@chopdawg.com>
2023-11-15 20:01:51 +0500

    resolved deployment issues

--- src/features/ChooseProject/index.tsx
+++ src/features/ChooseProject/index.tsx
@@ -1,5 +1,5 @@
-import { Box, Flex, Text, Title } from "@mantine/core";
-import React, { Fragment } from "react";
+import { Flex, Title } from "@mantine/core";
+import { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const ChooseProject = () => {
